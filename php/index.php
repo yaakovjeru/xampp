88dd1fd016c22410fbed509c5c9b787208858b84
Yaakov Suissa <yaakov_su@jerusalem.muni.il>
2023-02-28 11:21:18 +0200

    add files

---
+++ php/index.php
@@ -0,0 +1,46 @@
+<?php
+$handle = fopen ("file.csv","r");
+$counter = 0;
+$max = 60;
+
+while ($data = fgetcsv ($handle, 1000, ";") and ($counter < $max)) {
+        //$data = array_map("utf8_encode", $data); //added
+        $num = count ($data);
+        for ($c=0; $c < $num; $c++) {
+            $row = explode('|', $data[0]);
+            $mispar_mosah = $row[0];
+            $shem_mosah = $row[1];
+            $cod_sug_mosah = $row[2];
+            $sug_mosah = $row[3];
+            $ktovet = $row[4];
+            $yishuv = $row[5];
+            $telephone = $row[6];
+            $mikud = $row[7];
+            $cod_miktzoa = preg_replace("/[^0-9]/", "", $row[8]);
+            $miktzoa = $row[9];
+            $menahel_miktzoa = $row[10];
+            $rasham_havarot = $row[11];
+            if(in_array($cod_miktzoa, array(190, 194, 170, 172, 173, 21, 160))){
+                print_r($row);
+
+                echo $sql = "INSERT INTO list_professional (name, phone, address, ) VALUES ()";
+            }
+        }
+        $counter++;
+}
+
+// Array
+// (
+//     [0] => mispar_mosah
+//     [1] => shem_mosah
+//     [2] => cod_sug_mosah
+//     [3] => sug_mosah
+//     [4] => ktovet
+//     [5] => yishuv
+//     [6] => telephone
+//     [7] => mikud
+//     [8] => cod_miktzoa
+//     [9] => miktzoa
+//     [10] => menahel_miktzoa
+//     [11] => rasham_havarot
+// )
